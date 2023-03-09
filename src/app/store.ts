d566ec41b65c740142815b09e44b70cf8a9651fc
Traam <younesmrx@outlook.com>
2023-03-09 11:43:39 +0100

    added redux store config

---
+++ src/app/store.ts
@@ -0,0 +1,10 @@
+/** @format */
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+export type AppState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
